2a7c496413f97c7cbfa99e96e2b255bee7580062
RubinoD1 <Rubinod1@gmail.com>
2023-01-24 16:54:36 -0500

    added ID tags to all buttons and working on the DOM elements/testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,17 @@
 const output = document.querySelector('#output');
 
 const btnOne = document.querySelector('#button-one');
+const btnTwo = document.querySelector('#button-two');
+const btnThree = document.querySelector('#button-three');
+const btnFour = document.querySelector('#button-four');
+const btnFive = document.querySelector('#button-five');
+const btnSix = document.querySelector('#button-six');
+const btnSeven = document.querySelector('#button-seven');
+const btnEight = document.querySelector('#button-eight');
+const btnNine = document.querySelector('#button-nine');
+const btnZero = document.querySelector('#button-zero');
 
-
+const reset = document.querySelector('#reset');
 
 
 //Assign DOM pair to a its buttons value 
@@ -14,10 +23,15 @@ const btnOne = document.querySelector('#button-one');
 
 
 //onClick event console.log button to test it is properly linked. 
-btnOne.addEventListener('click', () => {
+btnTwo.addEventListener('click', () => {
     //the default behavior is that we are submitting on click (which flashes click for a sec in the console.log). 
     // To prevent the default behavior of the submit btn we take the event paramter and use preventDefault and call that method ()
     //e.preventDefault();
     output.innerHTML = '<h1>Click<h1>';
+}); 
 
+
+// RESET function 
+reset.addEventListener('click', () => {
+  output.innerHTML = '<h1><h1>';
 }); 
